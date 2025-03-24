950770dc098def23deaacafad9f34623116c185f
somir-khan <somirkhanshowrov@gmail.com>
2025-03-24 06:37:19 -0500

    root directory defined

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -6,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',  // 🔥 Add this line if your repo is somir-khan.github.io
   server: {
     host: "::",
     port: 8080,
